1ac537f44c04c33accadaa56a27cef5e61375498
Diego Cagnoni <diego@webheroes.it>
2015-09-18 13:03:12 +0200

    rm file per utente

--- admin/partials/wh-cc-creator-admin-cpt.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/**
-	 * This file is used to create custom post type.
-	 * In the future it will be afull functonal plugin.
-	 * For now use: https://generatewp.com/post-type/
-	 * and copy & paste the code here.
-	 */	
-		
--- admin/partials/wh-cc-creator-admin-tax.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/**
-	 * This file is used to create custom taxonomies.
-	 * In the future it will be afull functonal plugin.
-	 * For now use: https://generatewp.com/taxonomy/
-	 * and copy & paste the code here.
-	 */	
-	 	
